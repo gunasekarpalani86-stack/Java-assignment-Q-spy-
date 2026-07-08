73cdd3faf878f36d9577b15268c726cb5cb354ac
gunasekarpalani86-stack <gunasekarpalani86@gmail.com>
2026-07-08 21:12:40 +0530

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,16 @@
+class Food 
+{
+	public static void main(String[] args){
+		String ename = "Priya";
+	    double sal=25000 ;
+		
+		// Salary hiked 5000 increment //
+	    System.out.println("Employee name: " + ename) ;
+		
+		System.out.println("Previous salary is : " + sal + "Rs") ;
+		
+		sal+=5000;
+		
+		System.out.println("Current hiked salary  : " + sal + "Rs") ;
+	}
+}
