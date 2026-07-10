df312a5f975f663d9c928ef1b8d90bd46b474e6a
gunasekarpalani86-stack <gunasekarpalani86@gmail.com>
2026-07-10 19:28:11 +0530

    Implement student distribution calculation
    Calculates and prints the number of students per class based on total students and classes.

---
+++ Q6.java
@@ -0,0 +1,31 @@
+class mob 
+{
+	
+	//QUESTION Student Distribution
+	
+	/* A School has 240 Students 
+	 They are equally divived into 12 classes 
+	 Find the Number of students presented in each classes
+	
+	
+	Wajp To store and print the data */
+	
+	
+	public static void main(String[] args) 
+	{
+		int stu= 240;
+		int clas= 12;
+		
+		
+		System.out.println("The Totel number of the  Students  :" + stu);
+		System.out.println("Available Classes : " + clas );
+		
+		int sec= stu/clas;
+		
+		
+		System.out.println(sec + " Student for each Classes ");
+		
+	
+		
+	}
+}
