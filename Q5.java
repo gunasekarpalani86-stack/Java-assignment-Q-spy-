969b96ac15f9cab3b77d2c943a0ea9a26fc76577
gunasekarpalani86-stack <gunasekarpalani86@gmail.com>
2026-07-10 19:19:21 +0530

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,32 @@
+class mob 
+{
+	
+	//QUESTION Bonus salary
+	
+	/* Initially Kavya salary is 32000 rS
+	
+	She rechives a bonus 7000rs
+	
+	
+	Wajp To store and print the data */
+	
+	
+	public static void main(String[] args) 
+	{
+		String name="Kavya";
+		double sal =32000 ;
+		
+		System.out.println("Name of the Employee :" + name);
+		System.out.println("Basic salary :" + sal + " Rs " );
+		
+		double bon=7000;
+		
+		System.out.println("Bonus : "+ bon +  " Rs ");
+		
+		sal= sal+bon ;
+			
+		System.out.println("Totel Salary for this month is " + sal + " Rs");
+		
+		
+	}
+}
