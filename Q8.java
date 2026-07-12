51e2e0d8d1744d229bc7ec442715bb1f370bb903
gunasekarpalani86-stack <gunasekarpalani86@gmail.com>
2026-07-12 18:15:45 +0530

    Add cashback calculation program in Q8.java
    This Java program calculates and displays the account holder's name, initial balance, cashback received, and total balance after applying the cashback.

---
+++ Q8.java
@@ -0,0 +1,34 @@
+class mob 
+{
+	
+	//QUESTION Loptop Cahsback 
+
+	/* Initially kiran has 15000 rs in his wallet .
+	he receives a cashback of 2500 rs .
+	
+	
+	
+	Wajp To store and print the data */
+	
+	
+	public static void main(String[] args) 
+	{
+		double wal= 15000 ;
+		String name="Kiran";
+		double cb= 2500 ;
+		
+		
+		
+		System.out.println("Account holder name is  "  + name );
+		System.out.println("Account balance is :"  + wal + " Rs");
+		
+		System.out.println("Cashback received : " + cb );
+		
+		wal+=cb;
+		
+		System.out.println("Totel balance in your account is : "+ wal + " Rs");
+		
+	
+		
+	}
+}
